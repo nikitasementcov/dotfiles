bfa6ff24ef9088207d563a39391d1045ce0c9b72
Nikita Sementsov <sementcov.nikita@gmail.com>
2024-10-06 11:17:51 +0300

    add win-soft (copy from my gist)

---
+++ win-soft.sh
@@ -0,0 +1,41 @@
+# Install chocolatey - https://chocolatey.org/install
+
+# Better install manually:
+# choco install conemu -y
+# choso install alacritty -y
+# choco install googlechrome -y
+# choco install driverbooster -y
+
+# Main:
+choco install -y \
+ git winrar 7zip.install k-litecodecpackfull f.lux \
+ vscode postman vim microsoftazurestorageexplorer zoom notion \
+ firefox \
+ telegram \
+ # rdm
+ # aimp \
+ # adobereader
+ # dropbox \
+ # obsidian \
+ # virtualbox \
+ # nvm \
+ # slack \
+ # filezilla \
+ # qbittorrent \
+ powertoys \
+ 1password \
+ # jetbrainstoolbox \
+ ditto \
+ # rufus \
+ # opera \
+ # yandexdisk \
+
+# Check versions before installation:
+choco install -y \
+  vcredist2015 \
+  jre8 \
+  python2 \
+  python3 \
+
+# Install nerd font (for example Hack)
+# Use powertoys to remap casplock to ctrl
