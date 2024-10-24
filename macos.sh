b3f78de0d305122ce64ef40bc59f4aac9c3f8942
Nikita Sementsov <sementcov.nikita@gmail.com>
2024-10-24 21:58:08 +0300

    start writing of the macos.sh script

---
+++ macos.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Enable tap to click for the trackpad (restart could be required)
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+sudo defaults write /Library/Preferences/com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+
+# Move Dock to the left
+defaults write com.apple.dock orientation -string "left"
+
+
+# TODO:
+# hide dock
+# scroll direction on trackpad
+
+# Restarting
+killall SystemUIServer
+killall Dock
