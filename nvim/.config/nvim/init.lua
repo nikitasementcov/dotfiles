a8441a27e9c0c922322a632ba7a9b1333035edbd
Nikita Sementsov <sementcov.nikita@gmail.com>
2025-03-01 22:58:54 +0200

    vscode: add vscode settings, add vscode-nvim config entry point

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,39 +1,47 @@
-vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
-vim.g.mapleader = " "
+if vim.g.vscode then
+  -- VSCode extension:
 
--- bootstrap lazy and all plugins
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+  local map = vim.keymap.set
+  -- example:
+  -- map("n", "<C-w>gd", "<Cmd>lua require('vscode').action('editor.action.revealDefinitionAside')<CR>")
+else
+  vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
+  vim.g.mapleader = " "
 
-if not vim.uv.fs_stat(lazypath) then
-  local repo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+  -- bootstrap lazy and all plugins
+  local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+  if not vim.uv.fs_stat(lazypath) then
+    local repo = "https://github.com/folke/lazy.nvim.git"
+    vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+  end
+
+  vim.opt.rtp:prepend(lazypath)
+
+  local lazy_config = require "configs.lazy"
+
+  -- load plugins
+  require("lazy").setup({
+    {
+      "NvChad/NvChad",
+      lazy = false,
+      branch = "v2.5",
+      import = "nvchad.plugins",
+    },
+
+    { import = "plugins" },
+  }, lazy_config)
+
+  require("luasnip.loaders.from_lua").load { paths = "./lua/snippets/" }
+
+  -- load theme
+  dofile(vim.g.base46_cache .. "defaults")
+  dofile(vim.g.base46_cache .. "statusline")
+
+  require "options"
+  require "nvchad.autocmds"
+
+  vim.schedule(function()
+    require "mappings"
+  end)
 end
-
-vim.opt.rtp:prepend(lazypath)
-
-local lazy_config = require "configs.lazy"
-
--- load plugins
-require("lazy").setup({
-  {
-    "NvChad/NvChad",
-    lazy = false,
-    branch = "v2.5",
-    import = "nvchad.plugins",
-  },
-
-  { import = "plugins" },
-}, lazy_config)
-
-require("luasnip.loaders.from_lua").load { paths = "./lua/snippets/" }
-
--- load theme
-dofile(vim.g.base46_cache .. "defaults")
-dofile(vim.g.base46_cache .. "statusline")
-
-require "options"
-require "nvchad.autocmds"
-
-vim.schedule(function()
-  require "mappings"
-end)
