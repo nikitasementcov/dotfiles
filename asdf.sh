e2cbf49cdc49bbc3e30cbadeca1a592628cf8bea
Nikita Sementsov <sementcov.nikita@gmail.com>
2025-03-07 18:00:42 +0200

    asdf isntall python java rust

--- asdf.sh
+++ asdf.sh
@@ -8,3 +8,15 @@ brew install asdf
 asdf plugin add nodejs
 asdf install nodejs latest
 asdf global nodejs latest
+
+asdf plugin add python
+asdf install python latest
+asdf global python latest
+
+asdf plugin add rust
+asdf install rust latest
+asdf global rust latest
+
+asdf plugin add java
+asdf install java openjdk-17
+asdf global java openjdk-17
